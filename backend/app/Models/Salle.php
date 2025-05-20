f8c1f42c7649bf5daaba421b55c17eeb125d6e73
nmissi-nadia <nmissinadia@gmail.com>
2025-05-20 10:43:11 +0100

    configuration du Model Salle et cretion de controller concerné par Salle

--- backend/app/Models/Salle.php
+++ backend/app/Models/Salle.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Salle extends Model
 {
     use HasFactory;
+    protected $table = 'salles';
+    protected $fillable = [
+        'nom',
+        'description',
+        'images',
+        'responsable_id',
+        'location',
+        'capacite',
+        'status',
+    ];
+    public function responsable()
+    {
+        return $this->belongsTo(User::class, 'responsable_id');
+    }
+    public function disponibilites()
+    {
+        return $this->hasMany(Disponibilite::class);
+    }
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
 }
