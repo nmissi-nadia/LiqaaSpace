60c9d03e6173b494eeb938e1c8256d6cbc22e0b8
nmissi-nadia <nmissinadia@gmail.com>
2025-05-20 11:12:46 +0100

    Configuration de la Model disponibilite

--- backend/app/Models/Disponibilite.php
+++ backend/app/Models/Disponibilite.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Disponibilite extends Model
 {
     use HasFactory;
+    protected $table = 'disponibilites';
+    protected $fillable = [
+        'salle_id',
+        'date',
+        'heure_debut',
+        'heure_fin',
+        'statut',
+    ];
+    public function salle()
+    {
+        return $this->belongsTo(Salle::class);
+    }
 }
